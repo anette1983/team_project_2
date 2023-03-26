f76aa1de6e33ae1ba5b16eb1a11536430c42466d
MariaPetrukhnova <110037119+MariaPetrukhnova@users.noreply.github.com>
2023-03-26 21:03:20 +0300

    add task 2 to todos

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,14 +2,14 @@ import { Text } from 'components';
 import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
-export const Todo = ({ todo, index }) => {
+export const Todo = ({ todo, index, onDeleteClick }) => {
   return (
     <TodoWrapper>
       <Text textAlign="center" marginBottom="20px">
         Todo №{index}
       </Text>
       <Text>{todo.text}</Text>
-      <DeleteButton type="button">
+      <DeleteButton type="button" onClick ={() => onDeleteClick(todo.id)}>
         <RiDeleteBinLine size={24} />
       </DeleteButton>
     </TodoWrapper>
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -2,6 +2,8 @@ import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
+const LS_KEY = 'todos';
+
 export class Todos extends Component {
   state = {
     todos: [],
@@ -11,6 +13,25 @@ export class Todos extends Component {
     const newData = { id: nanoid(), text: value };
     this.setState(prevState => ({ todos: [...prevState.todos, newData] }));
   };
+
+  deleteTodos = (id) => {
+    this.setState(prevState => ({todos: prevState.todos.filter(todo => todo.id !== id)}))
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { todos } = this.state;
+    if (prevState.todos !== todos) {
+      localStorage.setItem(LS_KEY, JSON.stringify(todos));
+    }
+  }
+
+  componentDidMount() {
+    const savedTodos = JSON.parse(localStorage.getItem(LS_KEY));
+    if (savedTodos) {
+      this.setState({ todos: savedTodos });
+    }
+  }
+
   render() {
     return (
       <>
@@ -18,7 +39,7 @@ export class Todos extends Component {
         <Grid>
           {this.state.todos.map((todo, index) => (
             <GridItem key={todo.id}>
-              <Todo todo={todo} index={index + 1} />
+              <Todo todo={todo} index={index + 1} onDeleteClick={this.deleteTodos} />
             </GridItem>
           ))}
         </Grid>

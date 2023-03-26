abcb99f43a9f847cdc5cef2cc672109eec3eaac7
Hanna <anna_kon@bigmir.net>
2023-03-26 20:20:14 +0300

    update todos

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,19 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  }
+
+  onHandleSubmit = (value) => {
+    const newData = {id: nanoid(), text: value}
+    this.setState((prevState)=> ({ todos: [...prevState.todos, newData]
+    }))
+
+  }
   render() {
-    return <Text>Todos</Text>;
+    return (
+      <SearchForm onSubmit={this.onHandleSubmit}/>
+    )
   }
 }
